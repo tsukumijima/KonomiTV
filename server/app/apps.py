07d3f650076b381c4900f2dfa9f8ebba30fe6d64
tsukumijima <tsukumijima@users.noreply.github.com>
2021-07-09 05:56:31 +0900

    Update: 細かな調整

--- server/app/apps.py
+++ server/app/apps.py
@@ -1,5 +1,5 @@
-from django.apps import AppConfig
 
+from django.apps import AppConfig
 
 class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
--- server/app/serializers.py
+++ server/app/serializers.py
@@ -1,1 +1,4 @@
+
 from rest_framework import serializers
+
+# Create your serializers here.
