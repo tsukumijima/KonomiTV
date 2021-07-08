e17b3a82f6214480ff0f0ee99b42aec8406ed27e
tsukumijima <tsukumijima@users.noreply.github.com>
2021-07-09 03:44:25 +0900

    Update: アプリケーション名を app に変更 live と video でアプリを分けようと思ったけど、まだ方針が定まってないので一旦これで

--- server/app/apps.py
+++ server/app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class LiveConfig(AppConfig):
+class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'live'
+    name = 'app'
--- server/config/settings.py
+++ server/config/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'live',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -124,6 +124,7 @@ USE_TZ = True
 STATIC_URL = '/assets/'
 STATICFILES_DIRS = [BASE_DIR.parent / 'client/dist/assets']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
