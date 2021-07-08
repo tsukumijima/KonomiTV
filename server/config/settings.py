fdb2db98b8379fda7a1035d413b8ae44b0fb0bdf
tsukumijima <tsukumijima@users.noreply.github.com>
2021-07-09 03:40:19 +0900

    Update: SQLite データベースのファイル名を変更

--- server/config/settings.py
+++ server/config/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'config/db.sqlite3',
+        'NAME': BASE_DIR / 'config/database.sqlite',
     }
 }
 
