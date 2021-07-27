2f3be8269fa40e8b3ce474683353ea836132a8d1
tsukumijima <tsukumijima@users.noreply.github.com>
2021-07-27 11:21:09 +0900

    Update: /api/ 以下以外でファイルが存在しない場合、常に index.html を返すようにした SPA の history モードの場合、そうなっていないとうまく機能しないため /api/ 以下では 404 Not Found を返す

--- server/app/app.py
+++ server/app/app.py
@@ -3,14 +3,12 @@ import os
 import traceback
 from fastapi import FastAPI
 from fastapi import Request
-from fastapi import HTTPException
 from fastapi import status
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
 from fastapi.responses import JSONResponse
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
-from starlette.exceptions import HTTPException as StarletteHTTPException
 
 from app.constants import CLIENT_DIR
 from app.routers import streams
@@ -24,15 +22,19 @@ app = FastAPI(
     version='0.1.0',
 )
 
+# ルーターの設定
+app.include_router(streams.router)
+
 # 静的ファイルの設定
 app.mount('/assets', StaticFiles(directory=CLIENT_DIR / 'assets', html=True))
 
-## ルート以下の静的ファイルの配信の設定
-@app.get('/{filename}.{fileext}', include_in_schema=False)
-def root(filename:str, fileext:str):
+# ルート以下の静的ファイルの配信の設定
+@app.get('/{file:path}', include_in_schema=False)
+def root(file:str):
     # ファイルパス
-    filepath = CLIENT_DIR / f'{filename}.{fileext}'
+    filepath = CLIENT_DIR / f'{file}'
     if os.path.isfile(filepath):  # ファイルが存在する
+        fileext = file.split('.')[1]
         # MIMEタイプ
         if fileext == 'ico':
             mime = 'image/x-icon'
@@ -43,16 +45,19 @@ def root(filename:str, fileext:str):
         else:
             mime = 'text/plain'
         return FileResponse(CLIENT_DIR / filepath, media_type=mime)
-    else:  # 存在しない静的ファイルが指定された場合、index.html を返す
-        return HTMLResponse(open(CLIENT_DIR / 'index.html', encoding='utf-8').read())
+    else:  # 存在しない静的ファイルが指定された場合
+        Logging.info(file)
+        # パスに api/ が前方一致で含まれているなら、Not Found を返す
+        if file.startswith('api/'):
+            return JSONResponse({'detail': 'Not Found'}, status_code=status.HTTP_404_NOT_FOUND)
+        # パスに api/ が前方一致で含まれていなければ、index.html を返す
+        else:
+            return HTMLResponse(open(CLIENT_DIR / 'index.html', encoding='utf-8').read())
 
-## index.html の設定
+# index.html の設定
 @app.get('/', include_in_schema=False)
 def index():
     return HTMLResponse(open(CLIENT_DIR / 'index.html', encoding='utf-8').read())
-
-# ルーターの設定
-app.include_router(streams.router)
 
 # CORS の設定
 app.add_middleware(
@@ -66,23 +71,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# 404 Not found 時の設定
-# @app.exception_handler(StarletteHTTPException)
-# def notfound_handler(request:Request, exc: Exception):
-#     # 404 で、かつパスに /api/ が含まれていない
-#     Logging.error(exc.status_code)
-#     if exc.status_code == 404 and '/api/' not in str(request.url):
-#         return HTMLResponse(open(CLIENT_DIR / 'index.html', encoding='utf-8').read())
-#     # 404 で、かつパスに /api/ が含まれている
-#     else:
-#         return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={'detail': 'Not Found'})
-
 # 500 Internal server error の設定
 @app.exception_handler(Exception)
 def exception_handler(request:Request, exc: Exception):
     Logging.error(traceback.format_exc())
     if type(exc).__name__ == 'HTTPException':  # HTTPException を除外
         return JSONResponse(
+            {'detail': f'Oops! {type(exc).__name__} did something. There goes a rainbow...'},
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            content={'detail': f'Oops! {type(exc).__name__} did something. There goes a rainbow...'}
         )
