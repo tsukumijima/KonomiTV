ecc87dc6e372d0b42b4f9bfccd9e29965af4e5e8
tsukumijima <tsukumijima@users.noreply.github.com>
2021-07-29 12:17:32 +0900

    Fix: [Server] ルーティング設定の不具合を修正・微調整

--- server/app/app.py
+++ server/app/app.py
@@ -1,11 +1,9 @@
 
 import os
-import traceback
 from fastapi import FastAPI
 from fastapi import Request
 from fastapi import status
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import HTMLResponse
 from fastapi.responses import JSONResponse
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
@@ -22,7 +20,7 @@ app = FastAPI(
     version='0.1.0',
 )
 
-# ルーターの設定
+# ルーターの追加
 app.include_router(streams.router)
 
 # 静的ファイルの設定
@@ -33,31 +31,39 @@ app.mount('/assets', StaticFiles(directory=CLIENT_DIR / 'assets', html=True))
 def root(file:str):
     # ファイルパス
     filepath = CLIENT_DIR / f'{file}'
-    if os.path.isfile(filepath):  # ファイルが存在する
-        fileext = file.split('.')[1]
+    # ファイルが存在する
+    if os.path.isfile(filepath):
+        fileext = os.path.splitext(file)[1]
         # MIMEタイプ
-        if fileext == 'ico':
+        if fileext == '.css':
+            mime = 'text/css'
+        elif fileext == '.html':
+            mime = 'text/html'
+        elif fileext == '.ico':
             mime = 'image/x-icon'
-        elif fileext == 'js':
+        elif fileext == '.js':
             mime = 'application/javascript'
-        elif fileext == 'json':
+        elif fileext == '.json':
             mime = 'application/json'
         else:
             mime = 'text/plain'
-        return FileResponse(CLIENT_DIR / filepath, media_type=mime)
-    else:  # 存在しない静的ファイルが指定された場合
-        Logging.info(file)
-        # パスに api/ が前方一致で含まれているなら、Not Found を返す
+        return FileResponse(filepath, media_type=mime)
+    # デフォルトドキュメント (index.html)
+    elif os.path.isfile(filepath / 'index.html'):
+        return FileResponse(filepath / 'index.html', media_type='text/html')
+    # 存在しない静的ファイルが指定された場合
+    else:
+        # パスに api/ が前方一致で含まれているなら、404 Not Found を返す
         if file.startswith('api/'):
             return JSONResponse({'detail': 'Not Found'}, status_code=status.HTTP_404_NOT_FOUND)
         # パスに api/ が前方一致で含まれていなければ、index.html を返す
         else:
-            return HTMLResponse(open(CLIENT_DIR / 'index.html', encoding='utf-8').read())
+            return FileResponse(CLIENT_DIR / 'index.html', media_type='text/html')
 
 # index.html の設定
 @app.get('/', include_in_schema=False)
 def index():
-    return HTMLResponse(open(CLIENT_DIR / 'index.html', encoding='utf-8').read())
+    return FileResponse(CLIENT_DIR / 'index.html', media_type='text/html')
 
 # CORS の設定
 app.add_middleware(
@@ -74,9 +80,7 @@ app.add_middleware(
 # 500 Internal server error の設定
 @app.exception_handler(Exception)
 def exception_handler(request:Request, exc: Exception):
-    Logging.error(traceback.format_exc())
-    if type(exc).__name__ == 'HTTPException':  # HTTPException を除外
-        return JSONResponse(
-            {'detail': f'Oops! {type(exc).__name__} did something. There goes a rainbow...'},
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-        )
+    return JSONResponse(
+        {'detail': f'Oops! {type(exc).__name__} did something. There goes a rainbow...'},
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+    )
