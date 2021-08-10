3cdf5505d9aa0ce5ff02dbdfb632661c1084222d
tsukumijima <tsukumijima@users.noreply.github.com>
2021-08-10 10:25:51 +0900

    Update: [Server][Channels] チャンネル情報 API から現在と次の番組情報を取得できるように ただし現状非常に重たい（7秒近くかかる）ため何らかの改善が必要

--- server/app/routers/Channels.py
+++ server/app/routers/Channels.py
@@ -1,8 +1,13 @@
 
+import asyncio
+import time
 from fastapi import APIRouter
+from tortoise import timezone
 
 from app.models import Channels
+from app.models import Programs
 from app.schemas import ChannelsAPIResponse
+from app.utils import Logging
 
 
 # ルーター
@@ -16,17 +21,57 @@ router = APIRouter(
     '',
     summary = 'チャンネル情報 API',
     response_description = 'チャンネル情報。',
-    response_model = ChannelsAPIResponse,  # Response の構造を明示
 )
 async def ChannelsAPI():
     """
     地デジ (GR)・BS・CS・SKY それぞれのチャンネル情報を一括で取得する。
     """
 
-    # 各チャンネルタイプごとにチャンネル番号順で並び替えて表示
-    return {
-        'GR': await Channels.filter(channel_type='GR').order_by('channel_number').values(),
-        'BS': await Channels.filter(channel_type='BS').order_by('channel_number').values(),
-        'CS': await Channels.filter(channel_type='CS').order_by('channel_number').values(),
-        'SKY': await Channels.filter(channel_type='SKY').order_by('channel_number').values(),
+    # 現在時刻
+    now = timezone.now()
+
+    # チャンネル情報を取得
+    channels = await Channels.all().order_by('channel_number').values()
+
+    # レスポンスの雛形
+    result = {
+        'GR': list(),
+        'BS': list(),
+        'CS': list(),
+        'SKY': list(),
     }
+
+    # チャンネルごとに実行
+    tasks = list()
+    for channel in channels:
+
+        # 非同期並列実行するタスク
+        async def task(channel):
+
+            now = time.time()
+
+            # 現在の番組情報を取得する
+            ## 番組開始時刻が現在時刻と等しいかそれより前で、番組終了時刻が現在時刻よりも後
+            program_current = await Programs.filter(channel_id=channel['channel_id']).order_by('-start_time') \
+                                            .filter(start_time__lte = now, end_time__gt = now).first().values()
+            channel['program_current'] = program_current[0] if len(program_current) > 0 else None
+
+            # 次の番組情報を取得する
+            ## 番組開始時刻が現在時刻よりも後
+            program_next = await Programs.filter(channel_id=channel['channel_id']).order_by('start_time') \
+                                        .filter(start_time__gt = now).first().values()
+            channel['program_next'] = program_next[0] if len(program_next) > 0 else None
+
+            # チャンネルタイプで分類
+            result[channel['channel_type']].append(channel)
+
+            Logging.info('time:' + str(time.time() - now))
+
+        # タスク（コルーチン）を順次追加していく
+        tasks.append(task(channel))
+
+    # 一気に並列実行
+    await asyncio.gather(*tasks)
+
+    # チャンネルタイプごとに返却
+    return result
