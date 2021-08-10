8db9583c006caa9bb3ffeffe51053a2745c98d02
tsukumijima <tsukumijima@users.noreply.github.com>
2021-08-11 02:41:41 +0900

    Update: [Server][Channels] チャンネル情報 API の応答速度を 500ms 前後まで短縮 クエリが重いというよりかは 18000 件近くある大量のレコードを参照するのに時間がかかるようで、参照するレコードを絞ったらかなり早くなった

--- server/app/routers/Channels.py
+++ server/app/routers/Channels.py
@@ -1,6 +1,7 @@
 
 import asyncio
 import time
+from datetime import timedelta
 from fastapi import APIRouter
 from tortoise import timezone
 
@@ -39,12 +40,17 @@ async def ChannelsAPI():
     tasks.append(Channels.all().order_by('channel_number').values())
 
     # 現在の番組情報を取得する
-    ## 番組開始時刻が現在時刻と等しいかそれより前で、番組終了時刻が現在時刻よりも後
-    tasks.append(Programs.all().order_by('-start_time').filter(start_time__lte = now, end_time__gt = now).values())
+    tasks.append(Programs.all().order_by('-start_time').filter(
+        start_time__lte = now,  # 番組開始時刻が現在時刻と等しいかそれより前
+        end_time__gt = now,  # 番組終了時刻が現在時刻よりも後
+        end_time__lt = now + timedelta(hours=24),  # 番組終了時刻が(現在時刻 + 24時間)より前
+    ).values())
 
     # 次の番組情報を取得する
-    ## 番組開始時刻が現在時刻よりも後
-    tasks.append(Programs.all().order_by('start_time').filter(start_time__gt = now).values())
+    tasks.append(Programs.all().order_by('start_time').filter(
+        start_time__gt = now,  # 番組開始時刻が現在時刻よりも後
+        end_time__lt = now + timedelta(hours=24),  # 番組終了時刻が(現在時刻 + 24時間)より前
+    ).values())
 
     # 並列実行
     channels, programs_current, programs_next = await asyncio.gather(*tasks)
@@ -62,7 +68,6 @@ async def ChannelsAPI():
     now_t = time.time()
 
     # チャンネルごとに実行
-    tasks = list()
     for channel in channels:
 
         # チャンネル ID で絞り込む
