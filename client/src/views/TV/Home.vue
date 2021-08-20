39aa828ad281494715b65b1ad6e7e222cd07779f
tsukumijima <tsukumijima@users.noreply.github.com>
2021-08-21 00:47:31 +0900

    Add: [Client] チャンネルタブを作成

--- client/src/views/TV/Home.vue
+++ client/src/views/TV/Home.vue
@@ -3,6 +3,14 @@
         <Header/>
         <v-main>
             <Navigation/>
+            <div class="channels-container">
+                <v-tabs centered class="channels-tab">
+                    <v-tab class="channels-tab__item">ピン留め</v-tab>
+                    <v-tab class="channels-tab__item">地デジ</v-tab>
+                    <v-tab class="channels-tab__item">BS</v-tab>
+                    <v-tab class="channels-tab__item">CS</v-tab>
+                </v-tabs>
+            </div>
         </v-main>
     </div>
 </template>
@@ -30,3 +38,28 @@ export default Vue.extend({
     }
 });
 </script>
+
+<style lang="scss">
+.channels-container {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    margin: 12px 21px;
+
+    .channels-tab .v-tabs-bar {
+        height: 58px;
+        background: linear-gradient(to bottom, var(--v-background-base) calc(100% - 3px), var(--v-background-lighten1) 3px);  // 下線を引く
+
+        .v-tabs-slider-wrapper {
+            height: 3px !important;
+        }
+
+        .channels-tab__item {
+            width: 98px;
+            padding: 0;
+            color: var(--v-text-base) !important;
+            font-size: 16px;
+        }
+    }
+}
+</style>
