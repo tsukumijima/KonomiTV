47a9a1bb7b67be655b82a0ff062a9a86ae77c5c3
tsukumijima <tsukumijima@users.noreply.github.com>
2021-08-21 16:23:55 +0900

    Update: [Client] チャンネル周りのデザインを作成 だいたいピクセルパーフェクトのはず

--- client/src/views/TV/Home.vue
+++ client/src/views/TV/Home.vue
@@ -4,12 +4,54 @@
         <v-main>
             <Navigation/>
             <div class="channels-container">
-                <v-tabs centered class="channels-tab">
-                    <v-tab class="channels-tab__item">ピン留め</v-tab>
+                <v-tabs centered class="channels-tab" v-model="tab">
                     <v-tab class="channels-tab__item">地デジ</v-tab>
                     <v-tab class="channels-tab__item">BS</v-tab>
                     <v-tab class="channels-tab__item">CS</v-tab>
                 </v-tabs>
+                <v-tabs-items class="channels-list" v-model="tab">
+                    <v-tab-item class="channels">
+                        <div v-ripple class="channel">
+                            <div class="channel__broadcaster">
+                                <img class="channel__broadcaster-icon" src="http://localhost:7000/api/channels/gr011/logo">
+                                <div class="channel__broadcaster-content">
+                                    <span class="channel__broadcaster-name">Ch: 011 NHK総合1・東京</span>
+                                    <div class="channel__broadcaster-status">
+                                        <Icon icon="fa-solid:eye" height="12px" />
+                                        <span class="ml-1">0</span>
+                                        <Icon class="ml-4" icon="fa-solid:fire-alt" height="12px" />
+                                        <span class="ml-1">0</span>
+                                        <Icon class="ml-4" icon="bi:chat-left-text-fill" height="12px" />
+                                        <span class="ml-1">0</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="channel__program-present">
+                                <span class="channel__program-present-title">あさイチ「家事も人間関係もラクに！いま注目の“論理的思考”のススメ」</span>
+                                <span class="channel__program-present-time">2021/06/21 (月) 08:15 ～ 09:55 (105分)</span>
+                                <span class="channel__program-present-description">お笑いコンビ・和牛もおススメ！いま注目の“論理的思考法”とは？▼家事や人間関係もラクに…お金のかからない方法で段取り＆伝え方を変える！▼子どもが自発的に課題解決</span>
+                            </div>
+                            <v-spacer></v-spacer>
+                            <div class="channel__program-following">
+                                <div class="channel__program-following-title">
+                                    <span class="channel__program-following-title-decorate">NEXT</span>
+                                    <Icon class="channel__program-following-title-icon" icon="fluent:fast-forward-20-filled" width="16px" />
+                                    <span class="channel__program-following-title-text">NHKニュース おはよう日本“潜在看護師”接種業務にああああああ</span>
+                                </div>
+                                <span class="channel__program-following-time">2021/06/21 (月) 06:30 ～ 07:00 (30分)</span>
+                            </div>
+                            <div class="channel__progressbar">
+                                <div class="channel__progressbar-progress w-25"></div>
+                            </div>
+                        </div>
+                    </v-tab-item>
+                    <v-tab-item class="channels">
+                        <div>1aaaaaaa</div>
+                    </v-tab-item>
+                    <v-tab-item class="channels">
+                        <div>2aaaaaaa</div>
+                    </v-tab-item>
+                </v-tabs-items>
             </div>
         </v-main>
     </div>
@@ -17,6 +59,7 @@
 
 <script lang="ts">
 import Vue from 'vue';
+import { Icon } from '@iconify/vue2';
 import Header from '@/components/Header.vue';
 import Navigation from '@/components/Navigation.vue';
 
@@ -25,6 +68,12 @@ export default Vue.extend({
     components: {
         Header,
         Navigation,
+		Icon,
+    },
+    data() {
+        return {
+            tab: null,
+        }
     },
     created() {
         this.init();
@@ -46,19 +95,153 @@ export default Vue.extend({
     width: 100%;
     margin: 12px 21px;
 
-    .channels-tab .v-tabs-bar {
-        height: 58px;
-        background: linear-gradient(to bottom, var(--v-background-base) calc(100% - 3px), var(--v-background-lighten1) 3px);  // 下線を引く
+    .channels-tab {
+        flex: none;
 
-        .v-tabs-slider-wrapper {
-            height: 3px !important;
+        .v-tabs-bar {
+            height: 58px;
+            background: linear-gradient(to bottom, var(--v-background-base) calc(100% - 3px), var(--v-background-lighten1) 3px);  // 下線を引く
+
+            .v-tabs-slider-wrapper {
+                height: 3px !important;
+            }
+
+            .channels-tab__item {
+                width: 98px;
+                padding: 0;
+                color: var(--v-text-base) !important;
+                font-size: 16px;
+            }
         }
+    }
 
-        .channels-tab__item {
-            width: 98px;
-            padding: 0;
-            color: var(--v-text-base) !important;
-            font-size: 16px;
+    .channels-list {
+        margin-top: 26px;
+        background: var(--v-background-base) !important;
+
+        .channels {
+            display: flex;
+            flex-wrap: wrap;
+            gap: 18px;
+
+            .channel {
+                display: flex;
+                flex-direction: column;
+                position: relative;
+                width: 430px;
+                height: 275px;
+                padding: 18px 24px;
+                border-radius: 11px;
+                background: var(--v-background-lighten1);
+                overflow: hidden;  // progressbar を切り抜くために必要
+                user-select: none;
+                cursor: pointer;
+
+                .channel__broadcaster {
+                    display: flex;
+                    height: 44px;
+
+                    &-icon {
+                        display: inline-block;
+                        width: 80px;
+                        height: 44px;
+                        border-radius: 5px;
+                        object-fit: cover;
+                    }
+
+                    &-content {
+                        display: flex;
+                        flex-direction: column;
+                        margin-left: 16px;
+                    }
+
+                    &-name {
+                        font-size: 18px;
+                    }
+
+                    &-status {
+                        display: flex;
+                        align-items: center;
+                        margin-top: 2px;
+                        font-size: 12px;
+                        color: var(--v-text-darken1);
+                    }
+                }
+
+                .channel__program-present {
+                    display: flex;
+                    flex-direction: column;
+
+                    &-title {
+                        display: -webkit-box;
+                        margin-top: 14px;
+                        font-size: 16px;
+                        font-weight: 700;
+                        overflow: hidden;
+                        -webkit-line-clamp: 2;  // 2行までに制限
+                        -webkit-box-orient: vertical;
+                    }
+
+                    &-time {
+                        margin-top: 4px;
+                        color: var(--v-text-darken1);
+                        font-size: 13.5px;
+                    }
+
+                    &-description {
+                        display: -webkit-box;
+                        margin-top: 8px;
+                        color: var(--v-text-darken1);
+                        font-size: 10.5px;
+                        line-height: 175%;
+                        font-feature-settings: "palt" 1;  // 文字詰め
+                        letter-spacing: 0.06em;  // 字間を少し空ける
+                        overflow: hidden;
+                        -webkit-line-clamp: 3;  // 3行までに制限
+                        -webkit-box-orient: vertical;
+                    }
+                }
+
+                .channel__program-following {
+                    display: flex;
+                    flex-direction: column;
+                    color: var(--v-text-darken1);
+                    font-size: 12.5px;
+
+                    &-title {
+                       display: flex;
+                       align-items: center;
+                       &-decorate {
+                           flex-shrink: 0;
+                       }
+                       &-icon {
+                           flex-shrink: 0;
+                           margin-left: 3px;
+                       }
+                       &-text {
+                           margin-left: 3px;
+                            overflow: hidden;
+                            white-space: nowrap;
+                            text-overflow: ellipsis;  // はみ出た部分を … で省略
+                       }
+                    }
+                }
+
+                .channel__progressbar {
+                    position: absolute;
+                    left: 0;
+                    right: 0;
+                    bottom: 0;
+                    height: 4px;
+                    background: var(--v-gray-base);
+
+                    &-progress {
+                        height: 4px;
+                        background: var(--v-primary-base);
+                        transition: 0.3s ease;
+                    }
+                }
+            }
         }
     }
 }
