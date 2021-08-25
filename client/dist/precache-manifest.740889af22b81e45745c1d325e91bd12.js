b13425c70dd9e2c74c5c72cb220556f113450c91
tsukumijima <tsukumijima@users.noreply.github.com>
2021-08-25 11:03:20 +0900

    Add: [Client] dist を Git の管理下におくように 毎回ビルドしてると重くて仕方ないし、どの環境でも npm 諸々を整備しなくてはならなくなるため

---
+++ client/dist/precache-manifest.740889af22b81e45745c1d325e91bd12.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "408c8cf88962ae6f395c",
+    "url": "/assets/css/app.f7ff9a35.css"
+  },
+  {
+    "revision": "382d2db896c49bbf030d",
+    "url": "/assets/css/chunk-vendors.8aadd70e.css"
+  },
+  {
+    "revision": "408c8cf88962ae6f395c",
+    "url": "/assets/js/app.f4c383f4.js"
+  },
+  {
+    "revision": "382d2db896c49bbf030d",
+    "url": "/assets/js/chunk-vendors.34a4e978.js"
+  },
+  {
+    "revision": "6cde886bd4bed77137673f729eca6607",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18da3f2c4859633f8219b6eb3f822f71",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "7ea35bea8d7509b294e16697e05d4f19",
+    "url": "/mpegts.js/demo/demo.css"
+  },
+  {
+    "revision": "6e5f1f4619e1f246d8ff9eb8abfc34e0",
+    "url": "/mpegts.js/demo/index.html"
+  },
+  {
+    "revision": "2f27ef77c46723bcf60611a18ba94534",
+    "url": "/mpegts.js/dist/mpegts.ts"
+  },
+  {
+    "revision": "d98e9ec5fafad691048cbb603b5b0e71",
+    "url": "/robots.txt"
+  }
+]);
