bed81b435592fe5ef2829fe141b63ecbe7b096e1
tsukumijima <tsukumijima@users.noreply.github.com>
2021-08-29 14:57:19 +0900

    Update: [Server] Linux 環境でデータベースのマイグレーションに失敗する不具合を修正 どうやら SQLite は本来後から追加するカラムの NOT NULL 制約に必ずデフォルト値を設定しなければならないらしいのだが、Windows だとなぜかなくても通ってしまっていた

--- server/app/migrations/models/3_20210829063149_update.sql
+++ server/app/migrations/models/3_20210829063149_update.sql
@@ -1,6 +1,6 @@
 -- upgrade --
 ALTER TABLE "programs" RENAME COLUMN "audio_type" TO "primary_audio_type";
-ALTER TABLE "programs" ADD "primary_audio_language" TEXT NOT NULL;
+ALTER TABLE "programs" ADD "primary_audio_language" TEXT NOT NULL DEFAULT "";
 ALTER TABLE "programs" RENAME COLUMN "audio_sampling_rate" TO "primary_audio_sampling_rate";
 ALTER TABLE "programs" ADD "secondary_audio_type" TEXT;
 ALTER TABLE "programs" ADD "secondary_audio_language" TEXT;
