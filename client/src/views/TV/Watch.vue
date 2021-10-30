e0a905f9b55095b587d6777adcdff3157701ff7d
tsukumijima <tsukumijima@users.noreply.github.com>
2021-10-31 03:06:06 +0900

    Add: [Client][TV/Watch] チャンネル切り替えボタンにツールチップを追加

--- client/src/views/TV/Watch.vue
+++ client/src/views/TV/Watch.vue
@@ -48,16 +48,26 @@
                     </div>
                     <div class="watch-player__dplayer"></div>
                     <div class="watch-player__button">
-                        <router-link v-ripple class="switch-button switch-button-up" :to="`/tv/watch/${channel_previous.channel_id}`">
-                            <Icon class="switch-button-icon" icon="fluent:ios-arrow-left-24-filled" width="32px" rotate="1" />
-                        </router-link>
+                        <v-tooltip top transition="fade-transition">
+                            <template v-slot:activator="{ on }"><div v-on="on">
+                                <router-link v-ripple class="switch-button switch-button-up" :to="`/tv/watch/${channel_previous.channel_id}`">
+                                    <Icon class="switch-button-icon" icon="fluent:ios-arrow-left-24-filled" width="32px" rotate="1" />
+                                </router-link>
+                            </div></template>
+                            <span>前のチャンネル</span>
+                        </v-tooltip>
                         <div v-ripple class="switch-button switch-button-panel switch-button-panel--open"
                             @click="is_panel_visible = !is_panel_visible">
                             <Icon class="switch-button-icon" icon="fluent:navigation-16-filled" width="32px" />
                         </div>
-                        <router-link v-ripple class="switch-button switch-button-down" :to="`/tv/watch/${channel_next.channel_id}`">
-                            <Icon class="switch-button-icon" icon="fluent:ios-arrow-right-24-filled" width="33px" rotate="1" />
-                        </router-link>
+                        <v-tooltip bottom transition="fade-transition">
+                            <template v-slot:activator="{ on }"><div v-on="on">
+                                <router-link v-ripple class="switch-button switch-button-down" :to="`/tv/watch/${channel_next.channel_id}`">
+                                    <Icon class="switch-button-icon" icon="fluent:ios-arrow-right-24-filled" width="33px" rotate="1" />
+                                </router-link>
+                            </div></template>
+                            <span>次のチャンネル</span>
+                        </v-tooltip>
                     </div>
                 </div>
             </div>
@@ -876,6 +886,11 @@ export default mixins(mixin).extend({
 }
 .channels-container .v-tabs-bar {
     height: 48px;
+}
+.v-tooltip__content {
+    padding: 4px 10px !important;
+    background: #2F221FC0 !important;
+    font-size: 12px !important;
 }
 </style>
 
