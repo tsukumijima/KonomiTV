a6c4cc711b4a9e7d295df5c153f2f1a3964f63c0
tsukumijima <tsukumijima@users.noreply.github.com>
2021-11-02 19:56:31 +0900

    Update: [Client][VTabsItems] noImplicitThis を true にした際に表示されるエラーを修正 Vuetify 側で直接コンポーネントを拡張することが想定されていないらしく、型定義が VueConstructor だけでどうしようもなかった

--- client/src/components/VTabsItems.vue
+++ client/src/components/VTabsItems.vue
@@ -1,21 +1,20 @@
 <script lang="ts">
 
-import { VTabsItems } from 'vuetify/lib'
+import VTabsItems from 'vuetify/lib/components/VTabs/VTabsItems';
 
-export default {
-  extends: VTabsItems,
-  methods: {
+export default VTabsItems.extend({
+    methods: {
         // 最初のタブから最後のタブに遷移するとアニメーションの向きが逆になるバグがあるので、VWindow 側の挙動をオーバーライドする
         // 本来は VCarousel 用の動作だが、VTabsItems も VWindow を継承しているので、それが適用されてしまっているらしい
         // ref: https://github.com/yuwu9145/vuetify/blob/master/packages/vuetify/src/components/VWindow/VWindow.ts#L239-L252
         updateReverse (val: number, oldVal: number) {
-            const itemsLength = this.items.length
+            const itemsLength = (this as any).items.length
             const lastIndex = itemsLength - 1
 
-            if (itemsLength <= 2) return val < oldVal
+            if (itemsLength <= 2) return val < oldVal;
 
             // continuous が false の時、常に val < oldVal の結果を返す
-            if (!this.continuous) return val < oldVal;
+            if (!(this as any).continuous) return val < oldVal;
 
             if (val === lastIndex && oldVal === 0) {
                 return true
@@ -26,6 +25,6 @@ export default {
             }
         },
     }
-}
+});
 
 </script>
