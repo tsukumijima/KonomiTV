33abd8ad698720598943765ef580188dd26769ef
tsukumi <tsukumijima@users.noreply.github.com>
2022-02-11 08:48:05 +0900

    Fix: [Server] データベースのアップグレードを実行するとエラーが発生する問題を修正 Aerich が吐き出す SQL が SQLite の仕様 (ALTER TABLE で NOT NULL 制約を指定する際は DEFAULT 値を明示しなければならない) 上、実行できないものになってしまっているのが原因らしい ref: https://ytyaru.hatenablog.com/entry/2021/06/02/000000

--- server/app/migrations/models/6_20220129130828_update.sql
+++ server/app/migrations/models/6_20220129130828_update.sql
@@ -1,4 +1,4 @@
 -- upgrade --
-ALTER TABLE "channels" ADD "is_radiochannel" INT NOT NULL;
+ALTER TABLE "channels" ADD "is_radiochannel" INT NOT NULL DEFAULT 0;
 -- downgrade --
 ALTER TABLE "channels" DROP COLUMN "is_radiochannel";
