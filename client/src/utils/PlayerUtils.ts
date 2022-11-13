76da3ce3e8b3501f0e736115f4bba6df906e67c6
tsukumi <tsukumijima@users.noreply.github.com>
2022-11-14 05:43:05 +0900

    Update: [Client][PlayerUtils] 細かな修正

--- client/src/utils/PlayerUtils.ts
+++ client/src/utils/PlayerUtils.ts
@@ -17,10 +17,11 @@ export class PlayerUtils {
 
     /**
      * 現在のブラウザで H.265 / HEVC 映像が再生できるかどうかを取得する
+     * ref: https://github.com/StaZhu/enable-chromium-hevc-hardware-decoding#mediacapabilities
      * @returns 再生できるなら true、できないなら false
      */
     static isHEVCVideoSupported(): boolean {
-        // hvc1.1.1.L123.B0 の部分は呪文 (HEVC であることと、そのプロファイルを示す値らしい)
-        return document.createElement('video').canPlayType('video/mp4;codecs=hvc1.1.1.L123.B0') === 'probably';
+        // hvc1.1.6.L123.B0 の部分は呪文 (HEVC であることと、そのプロファイルを示す値らしい)
+        return document.createElement('video').canPlayType('video/mp4; codecs="hvc1.1.6.L123.B0"') === 'probably';
     }
 }
