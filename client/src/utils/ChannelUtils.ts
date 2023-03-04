e4fb41b307eaf941b7318677d0f724118b7bad3c
tsukumi <tsukumijima@users.noreply.github.com>
2023-03-04 01:10:41 +0000

    Remove: [Client][ChannelUtils] リファクタリングにより使用されなくなったメソッドを削除

--- client/src/utils/ChannelUtils.ts
+++ client/src/utils/ChannelUtils.ts
@@ -1,5 +1,5 @@
 
-import { ChannelType, ChannelTypePretty, IChannel } from '@/interface';
+import { ChannelType } from '@/services/Channels';
 
 /**
  * チャンネル周りのユーティリティ
@@ -9,23 +9,11 @@ export class ChannelUtils {
     /**
      * チャンネル ID からチャンネルタイプを取得する
      * @param channel_id チャンネル ID
-     * @param is_pretty ChannelTypePretty 型で返すかどうか
      * @returns チャンネルタイプ
      */
-    static getChannelType(channel_id: string, is_pretty: boolean = false): ChannelType | ChannelTypePretty {
+    static getChannelType(channel_id: string): ChannelType {
         const result = channel_id.match('(?<channel_type>[a-z]+)[0-9]+').groups.channel_type.toUpperCase();
-        if (is_pretty === true) {
-            switch (result) {
-                case 'GR':
-                    return '地デジ';
-                case 'STARDIGIO':
-                    return 'StarDigio';
-                default:
-                    return result as ChannelTypePretty;
-            }
-        } else {
-            return result as ChannelType;
-        }
+        return result as ChannelType;
     }
 
 
@@ -49,66 +37,5 @@ export class ChannelUtils {
 
         // それ以外
         return 'normal';
-    }
-
-
-    /**
-     * チャンネルタイプとリモコン番号からチャンネル情報を取得する
-     * @param channels_list チャンネルリスト
-     * @param channel_type チャンネルタイプ
-     * @param remocon_id リモコン番号
-     * @returns チャンネル情報
-     */
-    static getChannelFromRemoconID(channels_list: Map<ChannelTypePretty, IChannel[]>, channel_type: ChannelType, remocon_id: number): IChannel | null {
-
-        // ChannelTypePretty 型に変換する
-        const channel_type_pretty = channel_type.replace('GR', '地デジ').replace('STARDIGIO', 'StarDigio') as ChannelTypePretty;
-
-        // 指定されたチャンネルタイプのチャンネルを取得
-        const channels = channels_list.get(channel_type_pretty);  //「GR」は「地デジ」に置換してから取得
-
-        // リモコン番号が一致するチャンネルを見つけ、一番最初に見つかったものを返す
-        for (let index = 0; index < channels.length; index++) {
-            const channel = channels[index];
-            if (channel.remocon_id === remocon_id) {
-                return channel;
-            }
-        }
-
-        // リモコン番号が一致するチャンネルを見つけられなかった
-        return null;
-    }
-
-
-    /**
-     * 前・現在・次のチャンネル情報を取得する
-     * @param channels_list チャンネルリスト
-     * @param channel_id 起点にする現在のチャンネル ID
-     * @returns 前・現在・次のチャンネル情報
-     */
-    static getPreviousAndCurrentAndNextChannel(channels_list: Map<ChannelTypePretty, IChannel[]>, channel_id: string): IChannel[] {
-
-        // 前後のチャンネルを取得
-        const channels = channels_list.get(this.getChannelType(channel_id, true) as ChannelTypePretty);
-        for (let index = 0; index < channels.length; index++) {
-            const element = channels[index];
-
-            // チャンネル ID が一致したときだけ
-            if (element.channel_id === channel_id) {
-
-                // インデックスが最初か最後の時はそれぞれ最後と最初にインデックスを一周させる
-                let previous_index = index - 1;
-                if (previous_index === -1) previous_index = channels.length - 1;
-                let next_index = index + 1;
-                if (next_index === channels.length) next_index = 0;
-
-                // 前・現在・次のチャンネル情報を返す
-                return [channels[previous_index], channels[index], channels[next_index]];
-            }
-        }
-
-        // チャンネル ID が一致するチャンネルが見つからなかった
-        // そもそもここの処理に入る時点で何かがおかしい
-        return [null, null, null];
     }
 }
