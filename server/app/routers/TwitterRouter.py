34528e41c87e847b06e8cefd8b56f239e01e807f
tsukumi <tsukumijima@users.noreply.github.com>
2024-01-02 21:19:34 +0000

    Update: [Server][TwitterRouter] タイムライン API・検索 API を GraphQL API ベースで再実装 v1.1 の home_timeline こそまだ生きているがいつ死んでもおかしくないし、公式にはもう叩かれなくなっているはずなので怪しまれる可能性もある ラッパーを自作したので時間は掛かったが、仕様変更に強いコードになったと思う

--- server/app/routers/TwitterRouter.py
+++ server/app/routers/TwitterRouter.py
@@ -1,7 +1,6 @@
 
 import asyncio
 import json
-import re
 import tweepy
 import tweepy.models
 import tweepy.parsers
@@ -16,8 +15,7 @@ from fastapi import Query
 from fastapi import status
 from fastapi import UploadFile
 from tweepy_authlib import CookieSessionUserHandler
-from typing import Any, cast, Coroutine
-from zoneinfo import ZoneInfo
+from typing import Annotated, Any, cast, Coroutine
 
 from app import logging
 from app import schemas
@@ -35,8 +33,8 @@ router = APIRouter(
 
 
 async def GetCurrentTwitterAccount(
-    screen_name: str = Path(..., description='Twitter アカウントのスクリーンネーム。'),
-    current_user: User = Depends(GetCurrentUser),
+    screen_name: Annotated[str, Path(description='Twitter アカウントのスクリーンネーム。')],
+    current_user: Annotated[User, Depends(GetCurrentUser)],
 ) -> TwitterAccount:
     """ 現在ログイン中のユーザーに紐づく Twitter アカウントを取得する """
 
@@ -56,99 +54,14 @@ async def GetCurrentTwitterAccount(
     return twitter_account
 
 
-def GetCurrentTwitterAccountAPI(twitter_account: TwitterAccount = Depends(GetCurrentTwitterAccount)) -> tweepy.API:
-    """ 現在ログイン中のユーザーに紐づく Twitter アカウントの Tweepy インスタンスを取得する """
-    return twitter_account.getTweepyAPI()
-
-
-def RaiseHTTPException(ex: tweepy.HTTPException) -> None:
-    """ Twitter API のエラーコードからエラーメッセージを生成して HTTPException を発生させる """
-    if len(ex.api_codes) > 0 and len(ex.api_messages) > 0:
-        error_message = f'Code: {ex.api_codes[0]} / Message: {TwitterGraphQLAPI.ERROR_MESSAGES.get(ex.api_codes[0], ex.api_messages[0])}'
-    else:
-        error_message = f'Unknown Error (HTTP Error {ex.response.status_code})'
-    raise HTTPException(
-        status_code = status.HTTP_422_UNPROCESSABLE_ENTITY,
-        detail = f'Failed to Twitter API operation ({error_message})'
-    )
-
-
-def FormatTweet(tweet: tweepy.models.Status) -> schemas.Tweet:
-    """
-    Twitter API のツイートオブジェクトを API レスポンス用のツイートオブジェクトにフォーマットする
-
-    Args:
-        tweet (tweepy.models.Status): Twitter API のツイートオブジェクト
-
-    Returns:
-        schemas.Tweet: API レスポンス用にフォーマットしたツイートオブジェクト
-    """
-
-    # tweepy のモデルには型定義がないため、Any 型にキャスト
-    tweet_data = cast(Any, tweet)
-
-    # リツイートがある場合は、リツイート元のツイートの情報を取得
-    retweeted_tweet = None
-    if hasattr(tweet_data, 'retweeted_status'):
-        retweeted_tweet = FormatTweet(tweet_data.retweeted_status)
-
-    # 引用リツイートがある場合は、引用リツイート元のツイートの情報を取得
-    quoted_tweet = None
-    if hasattr(tweet_data, 'quoted_status'):
-        quoted_tweet = FormatTweet(tweet_data.quoted_status)
-
-    # 画像の URL を取得
-    image_urls = []
-    movie_url = None
-    if hasattr(tweet_data, 'extended_entities'):
-        for media in tweet_data.extended_entities['media']:
-            if media['type'] == 'photo':
-                image_urls.append(media['media_url_https'])
-            elif media['type'] in ['video', 'animated_gif']:
-                movie_url = media['video_info']['variants'][0]['url']  # bitrate が最も高いものを取得
-
-    # t.co の URL を展開した URL に置換
-    expanded_text = tweet_data.full_text
-    if hasattr(tweet_data, 'entities') and 'urls' in tweet_data.entities:
-        for url_entity in tweet_data.entities['urls']:
-            expanded_text = expanded_text.replace(url_entity['url'], url_entity['expanded_url'])
-
-    # 残った t.co の URL を削除
-    if len(image_urls) > 0 or movie_url:
-        expanded_text = re.sub(r'\s*https://t\.co/\w+$', '', expanded_text)
-
-    return schemas.Tweet(
-        id = tweet_data.id_str,
-        created_at = tweet_data.created_at.astimezone(ZoneInfo('Asia/Tokyo')),
-        user = schemas.TweetUser(
-            id = tweet_data.user.id_str,
-            name = tweet_data.user.name,
-            screen_name = tweet_data.user.screen_name,
-            # (ランダムな文字列)_normal.jpg だと画像サイズが小さいので、(ランダムな文字列).jpg に置換
-            icon_url = tweet_data.user.profile_image_url_https.replace('_normal', ''),
-        ),
-        text = expanded_text,
-        lang = tweet_data.lang,
-        via = re.sub(r'<.+?>', '', tweet_data.source),
-        image_urls = image_urls if len(image_urls) > 0 else None,
-        movie_url = movie_url,
-        retweet_count = tweet_data.retweet_count,
-        favorite_count = tweet_data.favorite_count,
-        retweeted = tweet_data.retweeted,
-        favorited = tweet_data.favorited,
-        retweeted_tweet = retweeted_tweet,
-        quoted_tweet = quoted_tweet,
-    )
-
-
 @router.post(
     '/password-auth',
     summary = 'Twitter パスワード認証 API',
     status_code = status.HTTP_204_NO_CONTENT,
 )
 async def TwitterPasswordAuthAPI(
-    password_auth_request: schemas.TwitterPasswordAuthRequest = Body(..., description='Twitter パスワード認証リクエスト'),
-    current_user: User = Depends(GetCurrentUser),
+    password_auth_request: Annotated[schemas.TwitterPasswordAuthRequest, Body(description='Twitter パスワード認証リクエスト')],
+    current_user: Annotated[User, Depends(GetCurrentUser)],
 ):
     """
     tweepy-authlib を利用してパスワード認証で Twitter 連携を行い、ログイン中のユーザーアカウントと Twitter アカウントを紐づける。
@@ -245,7 +158,7 @@ async def TwitterPasswordAuthAPI(
     status_code = status.HTTP_204_NO_CONTENT,
 )
 async def TwitterAccountDeleteAPI(
-    twitter_account: TwitterAccount = Depends(GetCurrentTwitterAccount),
+    twitter_account: Annotated[TwitterAccount, Depends(GetCurrentTwitterAccount)],
 ):
     """
     指定された Twitter アカウントの連携を解除する。<br>
@@ -287,12 +200,12 @@ async def TwitterAccountDeleteAPI(
     '/accounts/{screen_name}/tweets',
     summary = 'ツイート送信 API',
     response_description = 'ツイートの送信結果。',
-    response_model = schemas.TweetResult,
+    response_model = schemas.PostTweetResult | schemas.TwitterAPIResult,
 )
 async def TwitterTweetAPI(
-    tweet: str = Form('', description='ツイートの本文 (基本的には140文字までだが、プレミアムの加入状態や英数字の量に依存する) 。'),
-    images: list[UploadFile] = File([], description='ツイートに添付する画像 (4枚まで) 。'),
-    twitter_account: TwitterAccount = Depends(GetCurrentTwitterAccount),
+    twitter_account: Annotated[TwitterAccount, Depends(GetCurrentTwitterAccount)],
+    tweet: Annotated[str, Form(description='ツイートの本文 (基本的には140文字までだが、プレミアムの加入状態や英数字の量に依存する) 。')] = '',
+    images: Annotated[list[UploadFile], File(description='ツイートに添付する画像 (4枚まで) 。')] = [],
 ):
     """
     Twitter にツイートを送信する。ツイート本文 or 画像のみ送信することもできる。<br>
@@ -354,8 +267,8 @@ async def TwitterTweetAPI(
     response_model = schemas.TwitterAPIResult,
 )
 async def TwitterRetweetAPI(
-    tweet_id: str = Path(..., description='リツイートするツイートの ID。'),
-    twitter_account: TwitterAccount = Depends(GetCurrentTwitterAccount),
+    twitter_account: Annotated[TwitterAccount, Depends(GetCurrentTwitterAccount)],
+    tweet_id: Annotated[str, Path(description='リツイートするツイートの ID。')],
 ):
     """
     指定されたツイートをリツイートする。<br>
@@ -374,8 +287,8 @@ async def TwitterRetweetAPI(
     response_model = schemas.TwitterAPIResult,
 )
 async def TwitterRetweetCancelAPI(
-    tweet_id: str = Path(..., description='リツイートを取り消すツイートの ID。'),
-    twitter_account: TwitterAccount = Depends(GetCurrentTwitterAccount),
+    twitter_account: Annotated[TwitterAccount, Depends(GetCurrentTwitterAccount)],
+    tweet_id: Annotated[str, Path(description='リツイートを取り消すツイートの ID。')],
 ):
     """
     指定されたツイートのリツイートを取り消す。<br>
@@ -394,8 +307,8 @@ async def TwitterRetweetCancelAPI(
     response_model = schemas.TwitterAPIResult,
 )
 async def TwitterFavoriteAPI(
-    tweet_id: str = Path(..., description='いいねするツイートの ID。'),
-    twitter_account: TwitterAccount = Depends(GetCurrentTwitterAccount),
+    twitter_account: Annotated[TwitterAccount, Depends(GetCurrentTwitterAccount)],
+    tweet_id: Annotated[str, Path(description='いいねするツイートの ID。')],
 ):
     """
     指定されたツイートをいいねする。<br>
@@ -414,8 +327,8 @@ async def TwitterFavoriteAPI(
     response_model = schemas.TwitterAPIResult,
 )
 async def TwitterFavoriteCancelAPI(
-    tweet_id: str = Path(..., description='いいねを取り消すツイートの ID。'),
-    twitter_account: TwitterAccount = Depends(GetCurrentTwitterAccount),
+    twitter_account: Annotated[TwitterAccount, Depends(GetCurrentTwitterAccount)],
+    tweet_id: Annotated[str, Path(description='いいねを取り消すツイートの ID。')],
 ):
     """
     指定されたツイートのいいねを取り消す。<br>
@@ -431,11 +344,11 @@ async def TwitterFavoriteCancelAPI(
     '/accounts/{screen_name}/timeline',
     summary = 'ホームタイムライン取得 API',
     response_description = 'タイムラインのツイートのリスト。',
-    response_model = schemas.Tweets,
+    response_model = schemas.TimelineTweetsResult | schemas.TwitterAPIResult,
 )
 async def TwitterTimelineAPI(
-    since_tweet_id: str | None = Query(None, description='このツイート ID 以降のツイートを取得する。'),
-    twitter_account_api: tweepy.API = Depends(GetCurrentTwitterAccountAPI),
+    twitter_account: Annotated[TwitterAccount, Depends(GetCurrentTwitterAccount)],
+    cursor_id: Annotated[str | None, Query(description='前回のレスポンスから取得した、次のページを取得するためのカーソル ID 。')] = None,
 ):
     """
     ホームタイムラインを取得する。<br>
@@ -444,42 +357,19 @@ async def TwitterTimelineAPI(
     JWT エンコードされたアクセストークンがリクエストの Authorization: Bearer に設定されていないとアクセスできない。
     """
 
-    try:
-
-        # ホームタイムラインを取得
-        tweets = await asyncio.to_thread(twitter_account_api.home_timeline,
-            count = 200,
-            since_id = since_tweet_id,
-            trim_user = False,
-            exclude_replies = True,
-            include_entities = True,
-            tweet_mode = 'extended',
-        )
-
-        # レスポンス用に情報を整形
-        formatted_tweets: list[schemas.Tweet] = []
-        for tweet in tweets:
-
-            # 最終的なツイートモデルを作成
-            formatted_tweets.append(FormatTweet(tweet))
-
-        return formatted_tweets
-
-    except tweepy.HTTPException as ex:
-        RaiseHTTPException(ex)
+    return await TwitterGraphQLAPI(twitter_account).homeLatestTimeline(cursor_id, count=20)
 
 
 @router.get(
     '/accounts/{screen_name}/search',
-    summary='ツイート検索 API',
-    response_description='検索結果のツイートのリスト。',
-    response_model=schemas.Tweets,
+    summary = 'ツイート検索 API',
+    response_description = '検索結果のツイートのリスト。',
+    response_model = schemas.TimelineTweetsResult | schemas.TwitterAPIResult,
 )
 async def TwitterSearchAPI(
-    query: str = Query(..., description='検索クエリ。'),
-    since_tweet_id: str | None = Query(None, description='このツイート ID より後のツイートを取得する。'),
-    max_tweet_id: str | None = Query(None, description='このツイート ID より前のツイートを取得する。'),
-    twitter_account: TwitterAccount = Depends(GetCurrentTwitterAccount),
+    twitter_account: Annotated[TwitterAccount, Depends(GetCurrentTwitterAccount)],
+    query: Annotated[str, Query(description='検索クエリ。')],
+    cursor_id: Annotated[str | None, Query(description='前回のレスポンスから取得した、次のページを取得するためのカーソル ID 。')] = None,
 ):
     """
     指定されたクエリでツイートを検索する。
@@ -487,37 +377,4 @@ async def TwitterSearchAPI(
     JWT エンコードされたアクセストークンがリクエストの Authorization: Bearer に設定されていないとアクセスできない。
     """
 
-    # TODO: 現状動かないので書き直しが必要
-
-    # tweepy の API インスタンスを取得
-    api = twitter_account.getTweepyAPI()
-
-    try:
-
-        # ツイートを検索
-        tweets = api.search_tweets(
-            q = query.strip() + ' exclude:nativeretweets exclude:retweets exclude:replies',
-            count = 200,
-            since_id = since_tweet_id,
-            max_id = max_tweet_id,
-            lang = 'ja',
-            locale = 'ja',
-            result_type = 'recent',
-            include_entities = True,
-            tweet_mode = 'extended',
-        )
-
-        # レスポンス用に情報を整形
-        formatted_tweets: list[schemas.Tweet] = []
-        for tweet in tweets:
-
-            # 最終的なツイートモデルを作成
-            ## id が max_id と一致するツイートは除外する
-            formatted_tweet = FormatTweet(tweet)
-            if formatted_tweet.id != max_tweet_id:
-                formatted_tweets.append(formatted_tweet)
-
-        return formatted_tweets
-
-    except tweepy.HTTPException as ex:
-        RaiseHTTPException(ex)
+    return await TwitterGraphQLAPI(twitter_account).searchTimeline('Latest', query, cursor_id, count=20)
