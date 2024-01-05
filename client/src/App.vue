baaec9e72337df1291c44265f4818c4b4f37d1c1
tsukumi <tsukumijima@users.noreply.github.com>
2024-01-05 00:55:46 +0000

    Refactor: [Client] App.vue 内の SPA 全体に適用されるスタイル定義を整理

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,6 +7,66 @@
 </template>
 <style lang="scss">
 
+// ***** ブラウザのデフォルトスタイルの上書き *****
+
+// スクロールバーのスタイル
+* {
+    scrollbar-color: rgb(var(--v-theme-gray)) rgb(var(--v-theme-background));
+    scrollbar-width: thin;
+}
+::-webkit-scrollbar {
+    width: 7px;
+    height: 7px;
+}
+::-webkit-scrollbar-track {
+    background: rgb(var(--v-theme-background));
+}
+::-webkit-scrollbar-thumb {
+    background: rgb(var(--v-theme-background-lighten-2));
+    &:hover {
+        background: rgb(var(--v-theme-gray));
+    }
+}
+
+// Android でタップしたときのハイライトが出ないようにする
+* {
+    -webkit-tap-highlight-color: transparent;
+}
+
+// タッチデバイスで hover を無効にする
+@media (hover: none) {
+    :hover:before {
+        background-color: transparent !important;
+    }
+}
+
+// 選択時の色
+*::selection {
+	background-color: #E64F9780;
+}
+
+// リンクの既定の CSS をリセット
+a, a:link, a:visited, a:hover, a:active {
+    color: inherit;
+    text-decoration: none;
+}
+
+// ***** SPA 全体に適用されるスタイル *****
+
+// ページ遷移アニメーション
+.v-enter-active, .v-leave-active {
+    transition: opacity 0.25s;
+}
+.v-enter, .v-leave-to {
+    opacity: 0;
+}
+.v-enter-active.route-container {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+}
+
 // 全体のスタイル
 html {
     overflow-y: auto !important;
@@ -14,14 +74,19 @@ html {
 }
 
 // アプリケーションのルート
-body .v-application {
+.v-application {
     min-height: 100vh;
     min-height: 100dvh;
-    font-family: 'Open Sans', 'YakuHanJPs', 'Hiragino Sans', 'Noto Sans JP', sans-serif;
+    font-family: 'Open Sans', 'YakuHanJPs', 'Twemoji', 'Hiragino Sans', 'Noto Sans JP', sans-serif;
     font-weight: 500;
     overflow-x: clip;  // clip なら position: sticky; が効く
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+
+    // Safari は Twemoji COLR をうまく描画できない？ので当面無効にする
+    @supports (-webkit-touch-callout: none) {
+        font-family: 'Open Sans', 'YakuHanJPs', 'Hiragino Sans', 'Noto Sans JP', sans-serif;
+    }
 
     .v-application--wrap {
         min-height: 100% !important;
@@ -53,11 +118,65 @@ body .route-container {
     background: var(--v-background-base);
 }
 
+// ***** Vuetify 2 のスタイルの上書き *****
+
+// ツールチップのスタイル
+.v-popper--theme-tooltip {
+    .v-popper__inner {
+        display: inline-block;
+        padding: 4px 10px;
+        border-radius: 4px;
+        background: var(--v-background-lighten1);
+        color: var(--v-text-base);
+        font-size: 12px;
+        font-family: 'Open Sans', 'YakuHanJPs', 'Twemoji', 'Hiragino Sans', 'Noto Sans JP', sans-serif;
+        font-weight: 500;
+        opacity: 0.9;
+        line-height: 22px;
+    }
+    .v-popper__arrow-container {
+        display: none;
+    }
+}
+
+// ダイヤログ
+.v-dialog {
+    @include smartphone-vertical {
+        margin: 24px 6px !important;
+        &--fullscreen {
+            margin: 0 !important;
+        }
+    }
+}
+
 // ボタン内のテキストの字間をオフ
 // 大文字への強制変換をオフ
 .v-btn {
     letter-spacing: 0 !important;
     text-transform: none !important;
+}
+
+.v-card__text {
+    font-weight: inherit !important;
+}
+
+.v-tabs-items {
+    background-color: transparent !important;
+}
+
+.v-menu__content {
+    @include smartphone-vertical {
+        .v-list {
+            background-color: var(--v-background-lighten1) !important;
+        }
+    }
+    &::-webkit-scrollbar {
+        width: 12px;
+        height: 12px;
+    }
+    &::-webkit-scrollbar-thumb {
+        border: solid 3.5px var(--v-background-base);
+    }
 }
 
 .v-snack {
@@ -79,53 +198,9 @@ body .route-container {
     }
 }
 
-// ダイヤログ
-.v-dialog {
-    @include smartphone-vertical {
-        margin: 24px 6px !important;
-        &--fullscreen {
-            margin: 0 !important;
-        }
-    }
-}
+// ***** ユーティリティクラス *****
 
-// ツールチップのスタイル
-.v-popper--theme-tooltip {
-    .v-popper__inner {
-        display: inline-block;
-        padding: 4px 10px;
-        border-radius: 4px;
-        background: var(--v-background-lighten1);
-        color: var(--v-text-base);
-        font-size: 12px;
-        font-family: 'Open Sans', 'YakuHanJPs', 'Hiragino Sans', 'Noto Sans JP', sans-serif;
-        font-weight: 500;
-        opacity: 0.9;
-        line-height: 22px;
-    }
-    .v-popper__arrow-container {
-        display: none;
-    }
-}
-
-.v-tabs-items {
-    background-color: transparent !important;
-}
-
-// タッチデバイスで hover を無効にする
-// ref: https://github.com/vuetifyjs/vuetify/issues/2178
-@media (hover: none) {
-    :hover:before {
-        background-color: transparent !important;
-    }
-}
-
-// 選択時の色
-*::selection {
-	background-color: #E64F9780;
-}
-
-// 記号の装飾
+// 番組情報内の囲み文字の装飾
 .decorate-symbol {
     display: inline-flex;
     justify-content: center;
@@ -140,87 +215,17 @@ body .route-container {
     font-size: 0.94em;
 }
 
-// スクロールバー
-* {
-    scrollbar-color: var(--v-gray-base) var(--v-background-base);
-    scrollbar-width: thin;
-}
-::-webkit-scrollbar {
-    width: 7px;
-    height: 7px;
-}
-::-webkit-scrollbar-track {
-    background: var(--v-background-base);
-}
-::-webkit-scrollbar-thumb {
-    background: var(--v-background-lighten2);
-    &:hover {
-        background: var(--v-gray-base);
-    }
-}
-
-.v-menu__content {
+// スマホ縦画面 (幅が 600px 以下の端末) のみ表示する <br> タグ
+.br-smartphone-only {
+    display: none;
     @include smartphone-vertical {
-        .v-list {
-            background-color: var(--v-background-lighten1) !important;
-        }
-    }
-    &::-webkit-scrollbar {
-        width: 12px;
-        height: 12px;
-    }
-    &::-webkit-scrollbar-thumb {
-        border: solid 3.5px var(--v-background-base);
+        display: inline;
     }
 }
 
-.v-card__text {
-    font-weight: inherit !important;
-}
-
-// ページ遷移アニメーション
-.v-enter-active, .v-leave-active {
-    transition: opacity 0.25s;
-}
-.v-enter, .v-leave-to {
-    opacity: 0;
-}
-.v-enter-active.route-container {
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-}
-
-// カーソルをポインターにするやつ
+// カーソルをポインターにする
 .cursor-pointer {
     cursor: pointer;
-}
-
-// ユーティリティ
-.w-25 {
-    width: 25%;
-}
-.w-50 {
-    width: 50%;
-}
-.w-75 {
-    width: 75%;
-}
-.w-100 {
-    width: 100%;
-}
-.h-25 {
-    height: 25%;
-}
-.h-50 {
-    height: 50%;
-}
-.h-75 {
-    height: 75%;
-}
-.h-100 {
-    height: 100%;
 }
 
 </style>
