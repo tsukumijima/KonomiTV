ef2b28ee230d619d90138635ece7df2c1fe17f04
tsukumi <tsukumijima@users.noreply.github.com>
2025-02-03 02:02:00 +0900

    Fix: [Server][Utils/ProcessLimiter] 同時実行するプロセス数を CPU コア数の半分に制限する
    KeyFrameAnalyzer と ThumbnailGenerator を両方走らせてると i5-9400 環境ですら結構重いことが分かったので、他プロセスに影響しないように同時実行数を控えめにする

--- server/app/utils/ProcessLimiter.py
+++ server/app/utils/ProcessLimiter.py
@@ -6,7 +6,7 @@ from typing import ClassVar
 
 class ProcessLimiter:
     """
-    外部プロセスの同時実行数を CPU コア数に制限するためのユーティリティクラス
+    外部プロセスの同時実行数を CPU コア数の 50% に制限するためのユーティリティクラス
     """
 
     # クラス変数として Semaphore の辞書を保持
@@ -19,7 +19,7 @@ class ProcessLimiter:
     def getSemaphore(cls, process_key: str) -> asyncio.Semaphore:
         """
         指定されたプロセス用の Semaphore を取得する
-        初回呼び出し時に CPU 論理コア数分の Semaphore を作成する
+        初回呼び出し時に CPU 論理コア数の 50% の Semaphore を作成する
 
         Args:
             process_key (str): プロセスを識別するキー
@@ -33,5 +33,6 @@ class ProcessLimiter:
             cpu_count = psutil.cpu_count(logical=True)
             if cpu_count is None:
                 cpu_count = 4  # 取得できない場合は4コアと仮定
-            cls._semaphores[process_key] = asyncio.Semaphore(cpu_count)
+            # 同時実行数を CPU コア数の 50% に制限
+            cls._semaphores[process_key] = asyncio.Semaphore(cpu_count // 2)
         return cls._semaphores[process_key]
