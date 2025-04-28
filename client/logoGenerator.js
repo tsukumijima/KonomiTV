96353cda3d6b28748955bdc6defce0977a907290
huggy <i@huggy.moe>
2025-04-29 01:37:23 +0900

    Refactor: Improve code readability and consistency in logoGenerator.js

--- client/logoGenerator.js
+++ client/logoGenerator.js
@@ -1,20 +1,20 @@
-const Spritesmith = require("spritesmith");
-const sharp = require("sharp");
-const fs = require("fs");
-const { dirname } = require("path");
-const crypto = require("crypto");
-const baseDir = dirname(__dirname) + "/server/static/logos/";
+const crypto = require('crypto');
+const fs = require('fs');
+const { dirname } = require('path');
 
+const sharp = require('sharp');
+const Spritesmith = require('spritesmith');
+const baseDir = fs.existsSync('./logos') ? './logos/' : `${dirname(__dirname)}/server/static/logos/`;
 let channelMapper = {};
 
 fs.readdirSync(baseDir).forEach((filename) => {
-    if (filename !== "community-channels") {
-        channelMapper[filename.replace(".png", "")] = filename;
+    if (filename !== 'community-channels') {
+        channelMapper[filename.replace('.png', '')] = filename;
     }
 });
-fs.readdirSync(baseDir + "community-channels").forEach((filename) => {
-    channelMapper[filename.replace(".png", "")] =
-        "community-channels/" + filename;
+fs.readdirSync(baseDir + 'community-channels').forEach((filename) => {
+    channelMapper[filename.replace('.png', '')] =
+        'community-channels/' + filename;
 });
 let hashMapper = {};
 
@@ -29,7 +29,7 @@ let css = `.ch-sprite {
     --ch-sprite-real-height: 256;
     --ch-sprite-scale-ratio: calc(var(--ch-sprite-width) / var(--ch-sprite-real-width));
     --ch-sprite-real-croped-height: calc(var(--ch-sprite-height) / var(--ch-sprite-scale-ratio));
-  
+
     width: calc(var(--ch-sprite-real-width) * 1px);
     height: calc(var(--ch-sprite-real-croped-height) * 1px);
     background-position: calc(-1 * var(--ch-sprite-x) * 1px) calc(-1 * (var(--ch-sprite-y) + (var(--ch-sprite-real-height) - var(--ch-sprite-height) / var(--ch-sprite-scale-ratio)) / 2) * 1px);
@@ -76,7 +76,7 @@ async function run() {
                 //     // return ch === "default" || !ch.startsWith("NID3");
                 //     return true;
                 // })
-                .map((ch) => baseDir + channelMapper[ch]),
+                .map((ch) => baseDir + channelMapper[ch]).sort((a, b) => a > b),
         },
         async function (err, result) {
             if (err) throw err;
@@ -96,16 +96,16 @@ async function run() {
                 //                      with exploit LOL
                 css += `.ch-sprite[chid=${id}] {--ch-sprite-x: ${xy.x};--ch-sprite-y: ${xy.y};}`;
                 css += `.ch-sprite[chid=${id}] > img{display:none;}`;
-                css += "\n";
+                css += '\n';
             });
 
-            fs.writeFileSync(__dirname + "/src/styles/channel-logo.css", css);
+            fs.writeFileSync(__dirname + '/src/styles/channel-logo.css', css);
             console.log(channelXY);
-            sharp(result.image)
+            await sharp(result.image)
                 .png({ compressionLevel: 9 })
                 .webp({ quality: 80 })
                 .toFile(
-                    __dirname + "/public/assets/images/channel-logo-sprite.webp"
+                    __dirname + '/public/assets/images/channel-logo-sprite.webp'
                 );
         }
     );
@@ -114,6 +114,6 @@ run();
 
 async function hashFile(path) {
     const fileBuffer = fs.readFileSync(path);
-    const hash = crypto.createHash("sha256").update(fileBuffer).digest("hex");
+    const hash = crypto.createHash('sha256').update(fileBuffer).digest('hex');
     return hash;
 }
