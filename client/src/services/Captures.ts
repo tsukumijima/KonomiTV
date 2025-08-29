6ba3f2d5260071e64db5095263e17d9e28cb67ea
tsukumi <tsukumijima@users.noreply.github.com>
2025-08-30 01:42:39 +0900

    Fix: [Client][Services/Captures] 上り回線が細い環境でアップロードに失敗しやすい問題の緩和を試みる

--- client/src/services/Captures.ts
+++ client/src/services/Captures.ts
@@ -3,6 +3,7 @@ import { AxiosError } from 'axios';
 
 import Message from '@/message';
 import APIClient from '@/services/APIClient';
+import Utils from '@/utils';
 
 
 class Captures {
@@ -21,7 +22,12 @@ class Captures {
         form_data.append('image', blob, filename);
 
         // API リクエストを実行
-        const response = await APIClient.post('/captures', form_data, {headers: {'Content-Type': 'multipart/form-data'}});
+        const response = await APIClient.post('/captures', form_data, {
+            headers: {'Content-Type': 'multipart/form-data'},
+            // 回線状況によってはアップロードに時間がかかることがあるので、
+            // タイムアウトを 60 秒に伸ばす
+            timeout: 60 * 1000,
+        });
 
         // エラー処理
         if (response.type === 'error') {
@@ -41,19 +47,24 @@ class Captures {
                 default: {
                     if (Number.isNaN(response.status)) {
                         // HTTP リクエスト自体が失敗した場合はネットワークエラーの可能性が高い & 基本アップロードに失敗してはならないので、リトライする
+                        // リトライする前に 1 ~ 15 秒程度ランダムに待機することで、
+                        // リトライリクエストが同じタイミングで殺到するのを回避する
+                        const sleep_time_seconds = 1 + Math.random() * 15;
                         if (response.error.code === AxiosError.ECONNABORTED) {
                             // ネットワーク接続エラーの場合
-                            Message.warning('キャプチャのアップロード中にサーバーへの接続が切断されました。リトライします。');
+                            Message.warning(`キャプチャのアップロード中にサーバーへの接続が切断されました。${sleep_time_seconds} 秒後にリトライします。`);
                         } else if (response.error.code === AxiosError.ETIMEDOUT) {
                             // タイムアウトの場合
-                            Message.warning('キャプチャのアップロード中にサーバーへの接続がタイムアウトしました。リトライします。');
+                            Message.warning(`キャプチャのアップロード中にサーバーへの接続がタイムアウトしました。${sleep_time_seconds} 秒後にリトライします。`);
                         } else if (response.error.code === AxiosError.ERR_NETWORK) {
                             // 予期しないネットワークエラーの場合
-                            Message.warning('キャプチャのアップロード中に予期しないネットワークエラーが発生しました。リトライします。');
+                            Message.warning(`キャプチャのアップロード中に予期しないネットワークエラーが発生しました。${sleep_time_seconds} 秒後にリトライします。`);
                         } else {
                             // それ以外のエラーの場合
-                            Message.warning('キャプチャのアップロードに失敗しました。リトライします。');
+                            Message.warning(`キャプチャのアップロードに失敗しました。${sleep_time_seconds} 秒後にリトライします。`);
                         }
+                        await Utils.sleep(sleep_time_seconds);  // 秒単位で指定
+                        // リトライを実行
                         await this.uploadCapture(blob, filename);
                     } else {
                         APIClient.showGenericError(response, 'キャプチャのアップロードに失敗しました。');
