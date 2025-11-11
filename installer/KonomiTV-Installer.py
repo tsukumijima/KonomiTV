78ea2cdf7462ffff3be27ff2c59249c7fdbe1304
tsukumi <tsukumijima@users.noreply.github.com>
2025-11-11 22:14:51 +0900

    Update: [Installer] より説明がわかりやすくなるよう改善

--- installer/KonomiTV-Installer.py
+++ installer/KonomiTV-Installer.py
@@ -116,15 +116,18 @@ def main():
         print(Padding(table, (1, 2, 0, 2)))
 
     ShowPanel([
-        '01. KonomiTV をインストールするときは 1 を、アップデートするときは 2 を、',
-        '    アンインストールするときは 3 を入力してください。',
-        '    開発版 (4,5) には master ブランチの最新コミットが反映されています。',
-        '    安定動作は保証されておらずサポートもありませんので、ご了承の上ご利用ください。',
+        f'01. この PC 上に KonomiTV (version {TARGET_VERSION}) を[bold]新規インストール[/bold]するには [bold cyan]1[/bold cyan] を、',
+        f'    この PC 上の KonomiTV を version {TARGET_VERSION} へ[bold]アップデート[/bold]するには [bold cyan]2[/bold cyan] を、',
+        '    この PC 上の KonomiTV を[bold]アンインストール[/bold]するには [bold cyan]3[/bold cyan] を入力してください。',
+        '',
+        '    master ブランチの最新コミットが反映されている[bold]開発版をインストール[/bold]するには [bold cyan]4[/bold cyan] を、',
+        '    [bold]開発版へアップデート[/bold]するには [bold cyan]5[/bold cyan] を入力してください。',
+        '    なお、開発版の安定動作は保証されていないため、予めご了承の上ご利用ください。',
     ], padding=(1, 2, 1, 2))
 
     # 実行タイプ (インストール or アップデート or アンインストール)
     ## choices を指定することで、自動的にバリデーションが行われる（超便利）
-    run_type = int(CustomPrompt.ask('インストール(1) / アップデート(2) / アンインストール(3)\n  開発版をインストール(4) / 開発版にアップデート(5)', default='1', choices=['1', '2', '3', '4', '5']))
+    run_type = int(CustomPrompt.ask(f'v{TARGET_VERSION} をインストール (1) / v{TARGET_VERSION} へアップデート (2) / アンインストール (3)\n  開発版をインストール (4) / 開発版へアップデート (5)', default='1', choices=['1', '2', '3', '4', '5']))
 
     # Windows: コンソール出力前のおまじないとして、適当な PowerShell コマンドを実行する
     ## なぜ直るのかは全くもって謎だが、一度 PowerShell コマンドを実行しておくことで、print(Padding('Test', (1, 2, 0, 2)) のように
