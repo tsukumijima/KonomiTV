f48d84f2b1ea7dcc2ff53cb6e935c3cb6fbfd04a
tsukumi <tsukumijima@users.noreply.github.com>
2026-08-27 03:54:56 +0900

    Fix: [Client][Player/CaptureManager] mpeg2toh264 でオリジナル画質で再生中にキャプチャが撮れない問題を修正
    WebGL2 が描画バッファを都度破棄してしまうのが原因らしく（保持できなくもないがパフォーマンスが低下する）、mpeg2toh264/yadif モジュールにキャプチャ専用にデインタレース結果を描き直すメソッドを実装することで解決させた（疲れた…）
    デインタレースがオンかつ 1440×1080 の映像であっても、きちんと 16:9 でキャプチャできるようにしてある
    (デインタレースがオフの場合は HTMLVideoElement がキャプチャする & その場合 1440×1080 でも 16:9 を尊重して引き延ばしてくれるので問題ない)

--- client/src/services/player/managers/CaptureManager.ts
+++ client/src/services/player/managers/CaptureManager.ts
@@ -362,15 +362,15 @@ class CaptureManager implements PlayerManager {
 
         // ***** キャプチャの実行・字幕/文字スーパー/コメントを合成 *****
 
-        // YADIF Deinterlacer の動作中は Canvas が実際の表示映像を持つため、キャプチャ元も同じ Canvas へ切り替える
+        // YADIF Deinterlacer の動作中は、破棄可能な WebGL 描画バッファを専用 API で描き直してから取得する
         const deinterlacer = this.player.plugins.mpeg2toh264?.deinterlacer;
-        const video_source = deinterlacer instanceof Deinterlacer && deinterlacer.running === true ?
-            deinterlacer.canvas : this.player.video;
+        const capture_image_bitmap_promise = deinterlacer instanceof Deinterlacer && deinterlacer.running === true ?
+            deinterlacer.capture() : createImageBitmap(this.player.video);
 
         // 高速化のため、Promise.all() で並列に実行する
         const create_image_bitmap_results = await Promise.all([
             // 現在再生中の動画のキャプチャを ImageBitmap として取得
-            createImageBitmap(video_source),
+            capture_image_bitmap_promise,
             // 字幕が表示されていれば、字幕の Canvas を ImageBitmap として取得
             is_caption_showing ? createImageBitmap(caption_canvas) : null,
             // 文字スーパーが表示されていれば、文字スーパーの Canvas を ImageBitmap として取得
